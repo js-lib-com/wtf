c474652a4cf4cd3ed0dc5bcb06554758166b0c63
Iulian Rotaru <iulian@gnotis.ro>
2021-06-12 15:23:57 +0300

    Replace WebsUtil utility class with HttpRequest.

--- wtf-cli/src/main/java/com/jslib/wtf/cli/Update.java
+++ wtf-cli/src/main/java/com/jslib/wtf/cli/Update.java
@@ -2,6 +2,7 @@ package com.jslib.wtf.cli;
 
 import java.net.URI;
 import java.nio.file.Path;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -9,8 +10,10 @@ import java.util.regex.Pattern;
 import com.jslib.commons.cli.ExitCode;
 import com.jslib.commons.cli.Home;
 import com.jslib.commons.cli.IFile;
+import com.jslib.commons.cli.IProgress;
 import com.jslib.commons.cli.Task;
 
+import js.format.FileSize;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -21,6 +24,9 @@ public class Update extends Task {
 	private static final Pattern ARCHIVE_DIRECTORY_PATTERN = Pattern.compile("^\\d+\\.\\d+\\.\\d*(-[a-z0-9]+)?/$", Pattern.CASE_INSENSITIVE);
 	private static final Pattern ARCHIVE_FILE_PATTERN = Pattern.compile("^wtf-assembly.+\\.zip$");
 	private static final Pattern UPDATER_FILE_PATTERN = Pattern.compile("^wtf-update.+\\.jar$");
+
+	private static final DateTimeFormatter modificationTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+	private static final FileSize fileSizeFormatter = new FileSize();
 
 	@Option(names = { "-f", "--force" }, description = "Force update regardless release date.")
 	private boolean force;
@@ -35,14 +41,20 @@ public class Update extends Task {
 			console.print("Checking WTF assemblies repository...");
 		}
 
-		IFile assemblyDir = webs.latestVersion(DISTRIBUTION_URI, ARCHIVE_DIRECTORY_PATTERN, verbose);
+		IProgress<IFile> fileProgress = file -> {
+			if (verbose) {
+				console.print("%s %s\t%s", modificationTimeFormatter.format(file.getModificationTime()), fileSizeFormatter.format(file.getSize()), file.getName());
+			}
+		};
+
+		IFile assemblyDir = httpRequest.scanLatestFileVersion(DISTRIBUTION_URI, ARCHIVE_DIRECTORY_PATTERN, fileProgress);
 		if (assemblyDir == null) {
 			console.print("Empty WTF assemblies repository %s.", DISTRIBUTION_URI);
 			console.print("Command abort.");
 			return ExitCode.ABORT;
 		}
 
-		IFile assemblyFile = webs.latestVersion(assemblyDir.getURI(), ARCHIVE_FILE_PATTERN, verbose);
+		IFile assemblyFile = httpRequest.scanLatestFileVersion(assemblyDir.getURI(), ARCHIVE_FILE_PATTERN, fileProgress);
 		if (assemblyFile == null) {
 			console.print("Invalid WTF assembly version %s. No assembly found.", assemblyDir.getURI());
 			console.print("Command abort.");
@@ -76,7 +88,11 @@ public class Update extends Task {
 
 		console.print("Downloading WTF assembly %s...", assemblyFile.getName());
 		Path downloadFile = homeDir.resolve(assemblyFile.getName());
-		webs.download(assemblyFile, downloadFile, verbose);
+		httpRequest.download(assemblyFile.getURI(), downloadFile, length -> {
+			if (verbose) {
+				console.print('.');
+			}
+		});
 
 		console.print("Download complete. Start WTF install update.");
 		List<String> command = new ArrayList<>();
